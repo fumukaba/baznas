08e4f776e4bec43a56e640e4f30d67420164f948
fumukaba <fumukaba@gmail.com>
2019-01-03 16:21:25 +0700

    Update k4991560_neo.sql

--- k4991560_neo.sql
+++ k4991560_neo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Jan 2019 pada 09.30
+-- Waktu pembuatan: 03 Jan 2019 pada 10.20
 -- Versi server: 10.1.36-MariaDB
 -- Versi PHP: 7.2.11
 
@@ -46,7 +46,8 @@ CREATE TABLE `mainmenu` (
 
 INSERT INTO `mainmenu` (`seq`, `idmenu`, `nama_menu`, `active_menu`, `icon_class`, `link_menu`, `menu_akses`, `entry_date`, `entry_user`) VALUES
 (1, 1, 'Dashboard', '', 'menu-icon fa fa-tachometer', 'Dashboard', '', '2018-11-01 20:54:49', NULL),
-(24, 11, 'Master', '', 'menu-icon fa fa-money', '#', '', '2018-11-06 09:23:50', NULL);
+(24, 11, 'Master', '', 'menu-icon fa fa-file', '#', '', '2019-01-03 08:45:55', NULL),
+(27, 12, 'Tentang', '', 'menu-icon fa fa-question', 'About', '', '2019-01-03 08:47:29', NULL);
 
 -- --------------------------------------------------------
 
@@ -71,6 +72,8 @@ CREATE TABLE `submenu` (
 --
 
 INSERT INTO `submenu` (`id_sub`, `nama_sub`, `mainmenu_idmenu`, `active_sub`, `icon_class`, `link_sub`, `sub_akses`, `entry_date`, `entry_user`) VALUES
+(20, 'User Type', 11, '', '', 'User_type', '', '2019-01-03 09:11:36', NULL),
+(19, 'User', 11, '', '', 'User', '', '2019-01-03 09:11:40', NULL),
 (18, 'ZIS', 11, '', '', 'Zis', '', '2019-01-03 06:49:11', NULL);
 
 -- --------------------------------------------------------
@@ -96,6 +99,8 @@ CREATE TABLE `tab_akses_mainmenu` (
 --
 
 INSERT INTO `tab_akses_mainmenu` (`id`, `id_menu`, `id_level`, `c`, `r`, `u`, `d`, `entry_date`, `entry_user`) VALUES
+(45, 11, 6, 0, 1, 0, 0, '2019-01-03 09:15:52', ''),
+(44, 12, 5, 0, 1, 0, 0, '2019-01-03 08:47:05', ''),
 (43, 11, 5, 0, 1, 0, 0, '2019-01-03 06:55:46', '');
 
 -- --------------------------------------------------------
@@ -121,6 +126,8 @@ CREATE TABLE `tab_akses_submenu` (
 --
 
 INSERT INTO `tab_akses_submenu` (`id`, `id_sub_menu`, `id_level`, `c`, `r`, `u`, `d`, `entry_date`, `entry_user`) VALUES
+(38, 20, 6, 0, 1, 0, 0, '2019-01-03 09:12:40', ''),
+(37, 19, 6, 0, 1, 0, 0, '2019-01-03 09:12:40', ''),
 (36, 18, 5, 0, 1, 0, 0, '2019-01-03 06:56:55', '');
 
 -- --------------------------------------------------------
@@ -136,15 +143,12 @@ CREATE TABLE `tb_about` (
   `id_admin` int(11) DEFAULT NULL,
   `about_title_meta` varchar(200) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
   `about_deskripsi_meta` varchar(200) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
-  `about_keyword_meta` varchar(200) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL
+  `about_keyword_meta` varchar(200) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+  `nama_bank` varchar(50) NOT NULL,
+  `nomor_rekening` varchar(20) NOT NULL,
+  `atas_nama` varchar(50) NOT NULL,
+  `terakhir_diperbarui` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `tb_about`
---
-
-INSERT INTO `tb_about` (`id_about`, `about_logo`, `about_deskripsi`, `id_admin`, `about_title_meta`, `about_deskripsi_meta`, `about_keyword_meta`) VALUES
-(1, 'neo_wood_art.png', '<p>Tentang Neo Wood Art adalah</p>', 0, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -183,14 +187,8 @@ CREATE TABLE `tm_user` (
   `id_user` varchar(20) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `level` varchar(30) NOT NULL,
-  `status` varchar(1) NOT NULL,
   `foto` text,
   `password` varchar(100) NOT NULL,
-  `provinsi` varchar(50) NOT NULL,
-  `kota` varchar(50) NOT NULL,
-  `id_ongkir` int(11) NOT NULL,
-  `aktif_user` tinyint(1) NOT NULL,
   `nama_rek_user` varchar(255) NOT NULL,
   `no_rek_user` varchar(255) NOT NULL,
   `bank_rek_user` varchar(255) NOT NULL,
@@ -203,8 +201,9 @@ CREATE TABLE `tm_user` (
 -- Dumping data untuk tabel `tm_user`
 --
 
-INSERT INTO `tm_user` (`id_user`, `nama`, `email`, `level`, `status`, `foto`, `password`, `provinsi`, `kota`, `id_ongkir`, `aktif_user`, `nama_rek_user`, `no_rek_user`, `bank_rek_user`, `view_password`, `admin_level`, `id`) VALUES
-('baznas', 'Baznas', 'baznas@gmail.com', 'admin', '1', NULL, '596abd832ae81066c4cf716f6f70243c', 'Jawa Timur', 'Ponorogo', 0, 0, '', '', '', 'baznas', 5, 22);
+INSERT INTO `tm_user` (`id_user`, `nama`, `email`, `foto`, `password`, `nama_rek_user`, `no_rek_user`, `bank_rek_user`, `view_password`, `admin_level`, `id`) VALUES
+('baznas', 'Baznas', 'baznas@gmail.com', NULL, '596abd832ae81066c4cf716f6f70243c', '', '', '', 'baznas', 5, 22),
+('super', 'Super', 'super@gmail.com', NULL, '1b3231655cebb7a1f783eddf27d254ca', '', '', '', 'super', 6, 24);
 
 -- --------------------------------------------------------
 
@@ -223,7 +222,8 @@ CREATE TABLE `user_type` (
 --
 
 INSERT INTO `user_type` (`user_type_id`, `user_type_name`, `nama`) VALUES
-(5, 'Administrator', NULL);
+(5, 'Administrator', NULL),
+(6, 'Super Admin', NULL);
 
 --
 -- Indexes for dumped tables
@@ -288,25 +288,25 @@ ALTER TABLE `user_type`
 -- AUTO_INCREMENT untuk tabel `mainmenu`
 --
 ALTER TABLE `mainmenu`
-  MODIFY `seq` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `seq` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT untuk tabel `submenu`
 --
 ALTER TABLE `submenu`
-  MODIFY `id_sub` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_sub` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT untuk tabel `tab_akses_mainmenu`
 --
 ALTER TABLE `tab_akses_mainmenu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT untuk tabel `tab_akses_submenu`
 --
 ALTER TABLE `tab_akses_submenu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_about`
@@ -318,13 +318,13 @@ ALTER TABLE `tb_about`
 -- AUTO_INCREMENT untuk tabel `tm_user`
 --
 ALTER TABLE `tm_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT untuk tabel `user_type`
 --
 ALTER TABLE `user_type`
-  MODIFY `user_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
