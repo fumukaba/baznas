70534d2380ea7b318b34b69756525680a638f0ee
abdulhhabibie44 <46340801+abdulhhabibie44@users.noreply.github.com>
2019-01-05 11:06:47 +0700

    setting
    setting_new

--- application/controllers/Setting.php
+++ application/controllers/Setting.php
@@ -1,8 +1,8 @@
-<?php
+<?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Setting extends CI_Controller {
-
+	
 	public function __construct() {
 		parent::__construct();
 		$this->load->model('Mdl_setting');
@@ -11,20 +11,48 @@ class Setting extends CI_Controller {
 		$this->load->library("session");
 	}
 	
-	function index(){
-       // $this->mdl_home->getsqurity();
+	 function index(){
         $data['view_file']    = "moduls/setting";
         $this->load->view('admin_view',$data);
     }
-    
+	
+	
+	public function update() {
+        $tahun = $this->input->post('tahun');
+        $nominal = $this->input->post('nominal');
+        $query = $this->db->query("SELECT * FROM tb_setting WHERE tahun=$tahun AND meta_key='nominal_zakat_fitrah'")->result_array();
+        if(count($query)>0){
+            $this->db->update('tb_setting', 
+            array('tahun'=>$tahun,
+                  'meta_key'=>'nominal_zakat_fitrah',
+                  'meta_value'=>$nominal
+        ), array('meta_key'=>'nominal_zakat_fitrah','tahun'=>$tahun));
+
+        }
+        else {
+            $this->db->insert('tb_setting', 
+            array('id_setting'=>'',
+                  'tahun'=>$tahun,
+                  'meta_key'=>'nominal_zakat_fitrah',
+                  'meta_value'=>$nominal
+        ));
+        }
+		echo json_encode(array("status" => TRUE));
+    }
+
     public function ajax_edit() {
-		$data = $this->Mdl_setting->get_by_id();
-		//print_r($this->db->last_query());
+        $tahun = date('Y');
+        $query = $this->db->query("SELECT * FROM tb_setting WHERE tahun = $tahun AND meta_key='nominal_zakat_fitrah'")->result_array();
+        
+        if(count($query) == 0) {
+            $data['tahun'] = $tahun;
+            $data['meta_value'] = '';
+        } else {
+            $data['tahun'] = $query[0]['tahun'];
+            $data['meta_value'] = $query[0]['meta_value'];
+        }
 		echo json_encode($data);
 	}
+	
 
-	public function update_link() {
-		$data1 = array('fc_isi' => $this->input->post('waktu'));
-		$this->Mdl_setting->update_link($data1,array('fc_param' => 'WAKTU'));
-	}
 }	
--- application/views/moduls/setting.php
+++ application/views/moduls/setting.php
@@ -1,4 +1,4 @@
-<?php $title = "<i class='fa fa-cogs'></i>&nbsp;Setup Content"; ?>
+<?php $title = "<i class='fa fa-newspaper-o'></i>&nbsp;Setting"; ?>
 <div id="idImgLoader" style="margin: 0 auto; text-align: center;">
 	<img src='<?php echo base_url();?>assets/img/loader-dark.gif' />
 </div>
@@ -8,69 +8,185 @@
 	<h1>
 		<?php echo $title;?>
 	</h1>
-</div>
+</div><!-- /.page-header -->
+<style type="text/css"> #loader-upload{display: none}</style>
+<div id="form-update" >
 <div class="tabbable">
 	<ul class="nav nav-tabs" id="formAksi">
 		<li class="active">
-			<a data-toggle="tab" href="#home">
-			<i class="green ace-icon fa fa-file-image-o bigger-120"></i>
-				Header
+			<a data-toggle="tab" href="#zakatfitrah">
+			<i class="green ace-icon fa fa-home bigger-120"></i>
+				Zakat Fitrah
 			</a>
 		</li>
 
+		<li>
+			<a data-toggle="tab" href="#zakatmaal">
+			<i class="green ace-icon fa fa-money bigger-120"></i>
+				Zakat Maal
+			</a>
+		</li>
 
 											
 	</ul>
 
 	<div class="tab-content">
+	<div id="zakatfitrah" class="tab-pane fade in active">
+	<form action="#" id="form" name="form" class="form-horizontal" enctype="multipart/form-data">
 
-
-
-	
-	<div id="home" class="tab-pane fade in active">
-	<form id="formAksi" class="form-horizontal" role="form" action="#" method="POST" >
-		<div class="form-body">
-			<div class="form-group">
-			<label class="col-sm-2 control-label no-padding-right" for="form-field-1"> Waktu(Jam)</label>
-				<div class="col-sm-10">
-					<input type="text" id="waktu" name="waktu" class="col-xs-10 col-sm-5" />
-				</div>
-
-			</div>
-			
-			<div class="form-group">
-			
-			<div class="col-md-offset-2 col-md-9">
-				<button class="btn btn-info" type="submit" id="btn_save" onclick="save()">
-					<i class="ace-icon fa fa-check bigger-110"></i>
-					Update
-				</button>
-			</div>	
+		<input type="hidden" name="id_setting"/> 
+		<div class="form-group">
+		
+		<label class="col-sm-2 control-label no-padding-right" for="form-field-1"> Tahun </label>
+			<div class="col-sm-6">
+            <input type="text" id="tahun" name="tahun" placeholder="Tahun" readonly="readonly" value="<?php echo date('Y'); ?>" />
+				<span class="help-block"></span>
 			</div>
 		</div>
-	</form>	
+		<div class="form-group"> 
+
+		<label class="col-sm-2 control-label no-padding-right" for="form-field-1"> Nominal </label>
+			<div class="col-sm-6">
+				<input type="text" id="nominal" name="nominal" placeholder="Nominal" />
+			</div>	
+		</div>
+		<div class="form-group">
+		
+			<div class="col-md-offset-2 col-md-9">
+				<button type="button" value="Add" id="btnSave" onclick="save()" class="btn btn-primary">Save</button>
+				
+			</div>
+		</div>
+		
+	</form>
 	</div>
-	
-   
+
+	<div id="zakatmaal" class="tab-pane fade">
+	<!-- <form action="#" id="form" name="form" class="form-horizontal" enctype="multipart/form-data">
+
+		<input type="hidden" name="id_setting"/> 
+		<div class="form-group">
+		
+		<label class="col-sm-2 control-label no-padding-right" for="form-field-1"> Tahun </label>
+			<div class="col-sm-6">
+            <input type="text" id="tahun" name="tahun" placeholder="Tahun" readonly="readonly" />
+				<span class="help-block"></span>
+			</div>
+		</div>
+		<div class="form-group"> 
+
+		<label class="col-sm-2 control-label no-padding-right" for="form-field-1"> Nominal </label>
+			<div class="col-sm-6">
+				<input type="text" id="nominal" name="nominal" placeholder="Nominal" />
+			</div>	
+		</div>
+		<div class="form-group">
+		
+			<div class="col-md-offset-2 col-md-9">
+				<button type="button" value="Add" id="btnSave" onclick="save()" class="btn btn-primary">Save</button>
+				
+			</div>
+		</div>
+		
+	</form> -->
+	</div>
 
 	</div>
 </div>
-
 </div>
-</section>
 
-<script type="text/javascript">
+<script>
 	var zonk='';
+	var save_method;
 	var link = "<?php echo site_url('Setting')?>";
+	var table;
+	
 	$(document).ready(function(){
-      ubah();
+		setTimeout(function(){
+			update();
+		},3000)
     });
-
+	
 	function data(){
 		$('#data').fadeIn();
 	}
 	
+	$(document).ready(function(){
+	$('#form-add').submit(function(e) {
+		tinyMCE.triggerSave();
+		e.preventDefault(); var formData = new FormData($(this)[0]);
+		$.ajax({
+			url: $(this).attr("action"), type: 'POST', dataType: 'json', data: formData, async: true,
+			beforeSend: function() { $('#btnSave').text('saving...'); $('#btnSave').attr('disabled',true); },
+			success: function(response) {
+				if(response.status) { swal_berhasil(); update();
+				} else { Batal(); reload_table(); swal_error(response.error); }
+			},
+			complete: function() { $('#btnSave').text('save'); $('#btnSave').attr('disabled',false); },
+			cache: false, contentType: false, processData: false
+		});
+		return false;
+	});
 	
+	function readURL(input) {
+		$("#preview").show();
+		if (input.files && input.files[0]) {
+			var rd = new FileReader(); 
+			rd.onload = function (e) { $('#preview').attr('src', e.target.result); }; rd.readAsDataURL(input.files[0]);
+		}
+	}
+	$("#userfile").change(function(){ readURL(this); });
+
+	});
+	
+	$(document).ready(function(){
+		$('#form-upload').submit(function(e) {
+			tinyMCE.triggerSave();
+			e.preventDefault(); var formData = new FormData($(this)[0]);
+			$.ajax({
+				url: $(this).attr("action"), type: 'POST', dataType: 'json', data: formData, async: true,
+				beforeSend: function() { $('#btnSave').text('saving...'); $('#btnSave').attr('disabled',true); },
+				success: function(response) {
+					if(response.status) { swal_berhasil(); update();
+					} else { reload_table(); swal_error(response.error); }
+				},
+				complete: function() { $('#btnSave').text('save'); $('#btnSave').attr('disabled',false); },
+				cache: false, contentType: false, processData: false
+			});
+		});
+
+		function readURL(input) {
+			if (input.files && input.files[0]) {
+				var rd = new FileReader(); 
+				rd.onload = function (e) { $('#preview-upload').attr('src', e.target.result); }; rd.readAsDataURL(input.files[0]);
+			}
+		}
+		$("#file-upload").change(function(){ readURL(this); });
+	});
+	
+	function save() {
+		var url;
+		url = "<?= site_url()?>Setting/update/";
+		$('#btnSave').text('saving...'); $('#btnSave').attr('disabled',true);
+		tinyMCE.triggerSave();
+		$.ajax({
+			url : url, type: "POST", dataType: "JSON", data: $('#form').serialize(),
+			success: function(data) {
+				if(data.status) { swal_berhasil(); update();
+				} else {
+					for (var i = 0; i < data.inputerror.length; i++) {
+						$('[name="'+data.inputerror[i]+'"]').parent().parent().addClass('has-error'); 
+						$('[name="'+data.inputerror[i]+'"]').next().text(data.error_string[i]); 
+					}
+				}
+				$('#btnSave').text('save'); $('#btnSave').attr('disabled',false); 
+			},
+			error: function (jqXHR, textStatus, errorThrown) {
+				swal({ title:"ERROR", text:"Error adding / update data", type: "warning", closeOnConfirm: true}); 
+				$('#btnSave').text('save'); $('#btnSave').attr('disabled',false);  
+			}
+		});
+	}
 	
 	$(document).ready(function(){
       //$('#idImgLoader').show(2000);
@@ -78,7 +194,7 @@
 	  setTimeout(function(){
             data();
       }, 2000);
-	   setTimeout(function(){
+	  setTimeout(function(){
             ckeditor();
       }, 2000);
     });
@@ -89,48 +205,22 @@
 		});
 	}
 	
-    function ubah() {
-
+	function update() {
+			save_method = 'update';
+			$('#panel-data').fadeOut('slow');
+			$('#form-update').fadeIn('slow');
+			//document.getElementById('formAksi').reset();
+			$.ajax({
+				url : "<?php echo site_url('Setting')?>/ajax_edit/",
+				type: "GET",
+				dataType: "JSON",
+				success: function(result) {  
+				    $('[name="nominal"]').val(result.meta_value);
+				    $('[name="tahun"]').val(result.tahun);
+				}, error: function (jqXHR, textStatus, errorThrown) {
+					alert('Error get data from ajax');
+				}
+			});
+	}
 		
-		link_edit = "ajax_edit";
-		
-        $.ajax({
-            url : "<?php echo site_url('Setting')?>/"+link_edit+"/",
-            type: "GET",
-            dataType: "JSON",
-            success: function(result) {    
-			   $("input[name='waktu']").val(result.set_data1); 
-            }, error: function (jqXHR, textStatus, errorThrown) {
-                alert('Error get data from ajax');
-            }
-        });
-    }
-	
-    $(document).on('submit', '#formAksi', function(e) { 
-    tinyMCE.triggerSave(); 
-      e.preventDefault();
-			
-				link_edit = "update_link";
-		
-        $.ajax({
-            url : "<?php echo site_url('Setting')?>/"+link_edit+"/",
-            type: "POST",
-            data:  new FormData(this),
-            contentType: false,
-            cache: false,
-            processData:false,
-            success: function(data){                 
-                swal_berhasil(); 
-                ubah();
-                //$('#a7').val();  
-                setTimeout(function(){
-                    $('#btn_save').text('Ubah');
-                    $('#btn_save').attr('disabled', false);
-                    //document.getElementById('formAksi').reset();
-                }, 1000);
-
-            }           
-        });
-        return false;
-    });
-</script>
+</script>	
