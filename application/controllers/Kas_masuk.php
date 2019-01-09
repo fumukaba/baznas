b8898f35b9d77039e5d9de452db16e08d0350b94
nirwan04 <nirwanfirdaus4@gmail.com>
2019-01-09 15:31:24 +0700

    Iki rek seng uanyar pol
    meowmax

---
+++ application/controllers/Kas_masuk.php
@@ -0,0 +1,143 @@
+<?php  
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kas_masuk extends CI_Controller {
+	
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('Mdl_kasmas');
+		$this->auth->restrict();
+		date_default_timezone_set("Asia/Jakarta");
+		$this->load->library("session");
+	}
+	
+	 function index(){
+       // $this->mdl_home->getsqurity();
+        $data['view_file']    = "moduls/kasmas";
+        $this->load->view('admin_view',$data);
+    }
+	
+	public function ajax_list() {
+		$list = $this->Mdl_kasmas->get_datatables();
+		$data = array();
+		$no = $_REQUEST['start'];
+		foreach ($list as $kasmas) {
+
+		$asal_kasmas = $kasmas->asal_kasmas;			
+		if($asal_kasmas='Infaq'){
+			$tabel='tb_infaq';
+			$kolom='id_infaq';
+		}else if($asal_kasmas='Zakat Fitrah'){
+			$tabel='tb_zakat_fitrah';
+			$kolom='id_zakat_fitrah';
+		}else{
+			$tabel='tb_zakat_maal';
+			$kolom='id_zakat_maal';
+		}
+
+		$id_asal = $kasmas->id_asal;
+		$asal = $this->db->query("SELECT * FROM $tabel");
+		foreach($asal->result() as $row_asal)	{
+
+			$id_transaksi = $row_asal->$kolom; 		
+			if($id_asal==$id_transaksi){						
+					$dataRek_1=$row_asal->pemilik_rekening;
+					$dataRek_2=$row_asal->norek_pengirim;
+					$dataRek_3=$row_asal->bank_pengirim;
+					$id_zis=$row_asal->id_zis;
+				}
+			}			
+
+			$bahan_zis = $this->db->query("SELECT * FROM tb_zis");
+			foreach($bahan_zis->result() as $row_zis)	{
+	 		
+				if($id_zis==$row_zis->id_zis){						
+						$dataZis_1=$row_zis->nama_zis;
+						$dataZis_2=$row_zis->alamat_zis;
+					}
+				}			
+
+			$no++;
+			$row = array();
+			$row[] = '';
+			$row[] = $no;
+			$row[] = $kasmas->tanggal_kasmas;
+			$row[] = $dataRek_1 . "<br>" . $dataRek_2 . "<br>" . $dataRek_3;
+			$row[] = $dataZis_1 . "<br>" . $dataZis_2;
+			$row[] = $kasmas->asal_kasmas;
+			$row[] = $kasmas->jumlah_kasmas;
+			$row[] = '
+			<div class="btn-group">
+                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Aksi <span class="caret"></span></button>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="javascript:void(0)" onclick="edit('."'".$kasmas->id_kasmas."'".')">Edit</a></li>
+                            <li><a href="javascript:void(0)" onclick="hapus('."'".$kasmas->id_kasmas."'".')">Delete</a></li>
+                        </ul>
+            </div>';
+			$data[] = $row;
+		}
+
+		$output = array(
+						"draw" => $_REQUEST['draw'],
+						"recordsTotal" => $this->Mdl_kasmas->count_all(),
+						"recordsFiltered" => $this->Mdl_kasmas->count_filtered(),
+						"data" => $data,
+				);
+		echo json_encode($output);
+	}
+	
+	public function ajax_add() {
+
+        $id = $this->session->userdata('id');
+
+		$data = array(
+				'tanggal_kasmas'			=> $this->input->post('tanggal_kasmas'),
+				'asal_kasmas'         	=> $this->input->post('asal_kasmas'),
+				'id_asal'      			   	=> $this->input->post('asal_zis'),
+				'jumlah_kasmas'         	=> $this->input->post('jumlah_kasmas'),				
+				
+			);
+		$insert = $this->Mdl_kasmas->add($data);
+
+            // Kasbas
+            $jumlah_kaskel = $this->input->post('jumlah_kasmas');
+            $r_kasbas = $this->db->query("SELECT * FROM tb_kasbas ORDER BY id_kasbas DESC LIMIT 0, 1")->result_array();
+            $old_total = (count($r_kasbas) > 0 ? $r_kasbas[0]['total_kasbas'] : 0);
+            $new_total = $old_total - $jumlah_kasmas;
+
+            $kasbas = array(
+                'id_kasbas' => '',
+                'total_kasbas' => $new_total
+            );
+
+            $this->db->insert('tb_kasbas', $kasbas);		
+
+		//print_r($this->db->last_query());
+		echo json_encode(array('status' => TRUE));
+	}
+	
+	public function ajax_edit($id) {
+		$data = $this->Mdl_kasmas->get_by_id($id);
+		echo json_encode($data);
+	}
+	
+	public function ajax_update() {
+
+		$id = $this->session->userdata('id');
+
+		$data = array(
+            'tanggal_kasmas'			=> $this->input->post('tanggal_kasmas'),
+            'asal_kasmas'         	=> $this->input->post('asal_kasmas'),
+            'id_asal'      			   	=> $this->input->post('asal_zis'),
+            'jumlah_kasmas'         	=> $this->input->post('jumlah_kasmas'),					
+			);
+		$this->Mdl_kasmas->update(array('id_kasmas' => $this->input->post('id_kasmas')), $data);
+		echo json_encode(array("status" => TRUE));
+    }
+	
+	public function ajax_delete($id) {
+      $this->Mdl_kasmas->delete_by_id($id);
+      echo json_encode(array("status" => TRUE));
+    }
+
+}
---
+++ application/views/moduls/kasmas.php
@@ -0,0 +1,355 @@
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css"> 
+ <link href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css" rel="stylesheet" type="text/css" />
+ <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+ <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+ <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+ <script src="<?php echo base_url('assets/datetimepicker/js/bootstrap-datetimepicker.min.js') ?>"></script>
+ <link rel="stylesheet" href="<?php echo base_url('assets/datetimepicker/css/bootstrap-datetimepicker.min.css') ?>">
+
+ <style type="text/css">
+ 	
+ 	td.details-control {
+	    background: url('https://cdn.rawgit.com/DataTables/DataTables/6c7ada53ebc228ea9bc28b1b216e793b1825d188/examples/resources/details_open.png') no-repeat center center;
+	    cursor: pointer;
+	}
+	tr.shown td.details-control {
+	    background: url('https://cdn.rawgit.com/DataTables/DataTables/6c7ada53ebc228ea9bc28b1b216e793b1825d188/examples/resources/details_close.png') no-repeat center center;
+	}
+ </style>
+<?php $title = "<i class='fa fa-user'></i>&nbsp;Kas Masuk"; ?>
+<div class='alert alert-success' id='berhasil' style='display: none;'>Proses Berhasil</div>
+<div class='alert alert-danger' id='gagal' style='display: none;'>Proses Gagal</div>
+<div id="idImgLoader" style="margin: 0 auto; text-align: center;">
+	<img src='assets/img/loader-dark.gif' />
+</div>
+<div id="data" style="display:none;">
+<section class="content">
+<div class="page-header">
+	<h1>
+		<?php echo $title;?>
+	</h1>
+</div><!-- /.page-header -->
+
+<div id="panel-data">
+<div class="widget-box">
+<div class="widget-header">
+ 
+	<div class="widget-toolbar">
+		<a href="#" data-action="collapse">
+			<i class="ace-icon fa fa-chevron-up"></i>
+		</a>
+
+		<a href="#" data-action="close">
+			<i class="ace-icon fa fa-times"></i>
+		</a>
+	</div>
+	</div>
+
+<div class="widget-body">
+<div class="widget-main">
+<div class="row">
+<div class="col-xs-12">
+<div class="box-header">
+	<button class="btn btn-default" onclick="reload_table()"><i class="fa fa-refresh"></i> Reload</button>
+	<!-- <button class="btn btn-danger" onclick="Tambah()"><i class="fa fa-plus"></i> Tambah Data</button> -->
+</div><br />
+<table id="example" class="display responsive nowrap" cellspacing="0" width="100%">
+    <thead>
+        <tr>
+        	<th></th>
+            <th>No.</th>
+            <th>Tanggal Masuk</th>
+            <th>Pengirim</th>
+            <th>Tempat ZIS</th>
+            <th>Asal Kas</th>
+            <th>Jumlah Kas</th>
+        </tr>
+    </thead>
+    <tbody></tbody>
+</table>
+</div><!-- /.span -->
+</div>					
+</div><!-- /.row -->
+</div>
+</div>
+</div>
+
+</section>
+</div>
+<script>
+	var save_method;
+	var link = "<?php echo site_url('Kas_masuk')?>";
+	var table;
+	$(document).ready(function(){
+      //$('#idImgLoader').show(2000);
+	  $('#idImgLoader').fadeOut(2000);
+	  setTimeout(function(){
+            data();
+      }, 2000);
+    });
+	
+	function data(){
+		$('#data').fadeIn();
+	}
+	
+	$(document).ready(function() {
+		table = $('#example').DataTable({ 
+
+        "processing": true, //Feature control the processing indicator.
+        "serverSide": true, 
+        "order": [], //Initial no order.
+
+        // Load data for the table's content from an Ajax source
+        "ajax": {
+            "url": "<?php echo site_url('Kas_masuk/ajax_list')?>",
+            "type": "POST"
+        },
+
+        //Set column definition initialisation properties.
+        responsive: {
+            details: {
+                type: 'column'
+            }
+        },
+        columnDefs: [ {
+            className: 'control',
+            orderable: false,
+            targets:   0
+        } ],
+        order: [ 1, 'asc' ]
+
+    });
+	
+	});
+	
+	function reload_table() {
+    	table.ajax.reload(null, false);
+	}				
+
+	$(function() {
+		var oTable1 = $('#dynamic-table').dataTable( {
+		"aoColumns": [,
+			 null, null, null, null, null,
+		] } );
+				
+				
+		$('table th input:checkbox').on('click' , function(){
+			var that = this;
+			$(this).closest('table').find('tr > td:first-child input:checkbox')
+				.each(function(){
+					this.checked = that.checked;
+					$(this).closest('tr').toggleClass('selected');
+				});
+						
+			});
+			
+			
+		$('[data-rel="tooltip"]').tooltip({placement: tooltip_placement});
+			function tooltip_placement(context, source) {
+				var $source = $(source);
+				var $parent = $source.closest('table')
+				var off1 = $parent.offset();
+				var w1 = $parent.width();
+			
+				var off2 = $source.offset();
+				var w2 = $source.width();
+			
+				if( parseInt(off2.left) < parseInt(off1.left) + parseInt(w1 / 2) ) return 'right';
+				return 'left';
+		}
+	})
+			
+	//$('#dynamic-table').dataTable( {
+		//paging: false,
+		//searching: false
+	//} );	
+		
+	function reload_table() {
+		table.ajax.reload(null, false);
+	}
+	
+	function Batal() { 
+		$('#form-data').slideUp(500,'swing');
+		$('#panel-data').fadeIn(1000); 
+	}
+	
+	function Tambah() {
+		save_method = 'add'; 
+		$('#panel-data').fadeOut('slow');
+		$('#form-data').fadeIn('slow'); 
+		document.getElementById('formAksi').reset();
+	}
+	
+	function save() {
+			$('#btn_save').text('Saving...');
+			$('#btn_save').attr('disabled', true);
+
+			var url;
+			if (save_method == 'add') {
+				url = "<?php echo site_url('Kas_masuk')?>/ajax_add";
+			} else {
+				url = "<?php echo site_url('Kas_masuk')?>/ajax_update"; 
+			}
+
+			$.ajax({
+				url: url,
+				type: "POST",
+				data: $('#formAksi').serialize(),
+				dataType: "JSON",
+				success: function(result) {
+					if (result.status) {
+						
+							setTimeout(function(){
+								Batal();
+							}, 1000);
+						
+						setTimeout(function(){
+							reload_table();
+						}, 1000);
+					}
+					setTimeout(function(){
+						$('#btn_save').text('Save');
+						$('#btn_save').attr('disabled', false);
+						document.getElementById('formAksi').reset();
+					}, 1000);
+					swal_berhasil(); 
+					setTimeout(function(){
+							reload_table();
+					}, 1000);
+				}, error: function(jqXHR, textStatus, errorThrown) {
+					// alert('Error adding/update data');
+					swal({ title:"ERROR", text:"Error adding / update data", type: "warning", closeOnConfirm: true}); 
+					$('#btnSave').text('save'); $('#btnSave').attr('disabled',false);  
+				}
+			});
+	}
+	
+	function edit(id) {
+			save_method = 'update';
+			$('#panel-data').fadeOut('slow');
+			$('#form-data').fadeIn('slow');
+			document.getElementById('formAksi').reset();
+			$.ajax({
+				url : "<?php echo site_url('Kas_masuk')?>/ajax_edit/"+id,
+				type: "GET",
+				dataType: "JSON",
+				success: function(result) {  
+					//document.getElementById('fc_kdbahan').setAttribute('readonly','readonly');
+					$('[name="id_kaskel"]').val(result.id_kaskel);
+					$('[name="tanggal_kaskel"]').val(result.tanggal_kaskel);
+					$('[name="keperluan_kaskel"]').val(result.keperluan_kaskel);
+					$('[name="id_zis"]').val(result.id_zis);
+					$('[name="jumlah_kaskel"]').val(result.jumlah_kaskel);
+
+				}, error: function (jqXHR, textStatus, errorThrown) {
+					alert('Error get data from ajax');
+				}
+			});
+	}
+	
+	function hapus(id) {
+		if (confirm('Are you sure delete this data?')) {
+			$.ajax ({
+				url : "<?php echo site_url('Kas_masuk/ajax_delete')?>/"+id,
+				type: "POST",
+				dataType: "JSON",
+				success: function(data) {
+					setTimeout(function(){
+                        Batal();
+                    }, 1000);
+					
+					setTimeout(function(){
+                        reload_table();
+					}, 1000);
+					swal_berhasil(); 
+				}, error: function (jqXHR, textStatus, errorThrown) {
+					swal({ title:"ERROR", text:"Error delete data", type: "warning", closeOnConfirm: true}); 
+					$('#btnSave').text('save'); $('#btnSave').attr('disabled',false); 
+				}
+			});
+		}
+	}
+</script>
+
+<div class="row">
+<div class="col-xs-12">
+<div id="form-data" style="display:none;">
+<div class="widget-box">
+<div class="widget-header">
+		<h4 class="widget-title">Form Entry Kas keluar</h4>
+
+	<div class="widget-toolbar">
+		<a href="#" data-action="collapse">
+			<i class="ace-icon fa fa-chevron-up"></i>
+		</a>
+
+		<a onclick="Batal()" data-action="close">
+			<i class="ace-icon fa fa-times"></i>
+		</a>
+	</div>
+	</div>
+
+<div class="widget-body">
+<div class="widget-main">
+<div class="row">
+<div class="col-xs-12">
+<form class="form-horizontal" role="form" id="formAksi">
+	 <input type="hidden" name="id_kaskel">
+	<div class="form-group">
+	<label class="col-sm-2 control-label no-padding-right" for="form-field-1"> Tanggal </label>
+		<div class="col-sm-10">
+			<input type="text" id="tanggal_kaskel" name="tanggal_kaskel" placeholder="Contoh: 2018-12-31 20:15:00" class="col-xs-10 col-sm-5 form_datetime" />
+		</div>
+	</div>
+
+	<div class="form-group">
+	<label class="col-sm-2 control-label no-padding-right" for="form-field-1"> Keperluan </label>
+		<div class="col-sm-10">
+			<input type="text" id="keperluan_kaskel" name="keperluan_kaskel" placeholder="Keperluan" class="col-xs-10 col-sm-5" />
+		</div>
+	</div>
+
+	<div class="form-group">
+	<label class="col-sm-2 control-label no-padding-right" for="form-field-1"> Tempat ZIS </label>
+		<div class="col-sm-4">
+        <select name="id_zis" id="id_zis" class="form-control">
+			<?php 
+            $zis = $this->db->query("SELECT * FROM tb_zis");
+			foreach($zis->result() as $row_kat)	{	?>
+				<option class="c<?php echo $row_kat->id_zis; ?>" value="<?php echo $row_kat->id_zis?>"><?php echo $row_kat->nama_zis; ?></option>
+			<?php } ?>
+			</select>
+		</div>
+	</div>
+
+	<div class="form-group">
+	<label class="col-sm-2 control-label no-padding-right" for="form-field-1"> Jumlah </label>
+		<div class="col-sm-10">
+			<input type="text" id="jumlah_kaskel" name="jumlah_kaskel" placeholder="Jumlah Kas Keluar" class="col-xs-10 col-sm-5" />
+		</div>
+	</div>
+
+	</div>
+	<div class="col-md-offset-2 col-md-9">
+				<button class="btn btn-info" type="button" id="btn_save" onclick="save()">
+					<i class="ace-icon fa fa-check bigger-110"></i>
+					Submit
+				</button>
+
+				&nbsp; &nbsp; &nbsp;
+				<button class="btn" type="reset">
+				<i class="ace-icon fa fa-undo bigger-110"></i>
+					Reset
+				</button>
+	</div>
+</form>
+</div>
+</div>
+</div>					
+</div><!-- /.row -->
+</div>
+</div><!-- /.row -->
+</div>
+<script type="text/javascript">
+    $(".form_datetime").datetimepicker({format: 'yyyy-mm-dd hh:ii'});
+</script> 
