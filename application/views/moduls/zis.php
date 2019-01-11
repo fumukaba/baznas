0c69b30fae6cf1f7993bb85a01477c950008fd09
abdulhhabibie44 <46340801+abdulhhabibie44@users.noreply.github.com>
2019-01-11 13:56:21 +0700

    huh
    iki wan

--- application/views/moduls/zis.php
+++ application/views/moduls/zis.php
@@ -12,7 +12,7 @@
 	}
 	tr.shown td.details-control {
 	    background: url('https://cdn.rawgit.com/DataTables/DataTables/6c7ada53ebc228ea9bc28b1b216e793b1825d188/examples/resources/details_close.png') no-repeat center center;
-	} 
+	}  
  </style>
 <?php $title = "<i class='fa fa-map-marker'></i>&nbsp;Tempat ZIS"; ?>
 
